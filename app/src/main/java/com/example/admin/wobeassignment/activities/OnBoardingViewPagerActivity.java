00e826818281e0a221f45145195979ff4585068f
Meghana <meghanashastri07@gmail.com>
2017-09-19 19:19:36 +0530

    temporary commit

--- app/src/main/java/com/example/admin/wobeassignment/activities/OnBoardingViewPagerActivity.java
+++ app/src/main/java/com/example/admin/wobeassignment/activities/OnBoardingViewPagerActivity.java
@@ -2,8 +2,6 @@ package com.example.admin.wobeassignment.activities;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 
 import com.example.admin.wobeassignment.R;
