123b04908bb567422c98903e329769fb40a2d828
Meghana <meghanashastri07@gmail.com>
2017-09-20 14:55:37 +0530

    google sign in added in register activity

--- app/src/main/java/com/example/admin/wobeassignment/activities/RegisterActivity.java
+++ app/src/main/java/com/example/admin/wobeassignment/activities/RegisterActivity.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.admin.wobeassignment.R;
+import com.example.admin.wobeassignment.fragments.GoogleSignInFragment;
 import com.example.admin.wobeassignment.fragments.RegisterFragment;
 
 /**
  * Created by Admin on 20-09-2017.
  */
 
-public class RegisterActivity extends FragmentActivity {
+public class RegisterActivity extends FragmentActivity implements View.OnClickListener {
 
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
@@ -22,7 +25,14 @@ public class RegisterActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         addFragment();
+        initialiseViews();
     }
+
+    private void initialiseViews() {
+        Button btnGoogleSignUp = (Button) findViewById(R.id.btnGoogleSignUp);
+        btnGoogleSignUp.setOnClickListener(this);
+    }
+
 
     public void addFragment() {
         fragmentManager = getSupportFragmentManager();
@@ -30,5 +40,23 @@ public class RegisterActivity extends FragmentActivity {
         RegisterFragment registerFragment = new RegisterFragment();
         fragmentTransaction.add(R.id.fragment_holder, registerFragment, "Register");
         fragmentTransaction.commit();
+    }
+
+    public void replaceFragment() {
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        GoogleSignInFragment googleSignInFragment = new GoogleSignInFragment();
+        fragmentTransaction.replace(R.id.fragment_holder, googleSignInFragment, "GoogleSignIn");
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btnGoogleSignUp:
+                replaceFragment();
+                break;
+        }
     }
 }
