3a19154829dd4936e16045311da9eb5c1b5edbfc
Meghana <meghanashastri07@gmail.com>
2017-09-20 18:05:27 +0530

    facebook login done in login screen

--- app/src/main/java/com/example/admin/wobeassignment/activities/LoginActivity.java
+++ app/src/main/java/com/example/admin/wobeassignment/activities/LoginActivity.java
@@ -1,15 +1,33 @@
 package com.example.admin.wobeassignment.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.example.admin.wobeassignment.R;
 import com.example.admin.wobeassignment.fragments.GoogleSignInFragment;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Admin on 19-09-2017.
@@ -19,13 +37,92 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
 
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    Button fb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         super.onCreate(savedInstanceState);
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
+
+
+        fb = (Button) findViewById(R.id.btnFacebookSignIn);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        fb.setOnClickListener(this);
+
+        loginButton.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                        Log.d("success", "Success");
+
+                        String accessToken = loginResult.getAccessToken()
+                                .getToken();
+                        Log.i("accessToken", accessToken);
+
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(),
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject object,
+                                                            GraphResponse response) {
+
+                                        Log.i("LoginActivity",
+                                                response.toString());
+                                        try {
+                                            String id = object.getString("id");
+                                            try {
+                                                URL profile_pic = new URL(
+                                                        "http://graph.facebook.com/" + id + "/picture?type=large");
+                                                Log.i("profile_pic",
+                                                        profile_pic + "");
+
+                                            } catch (MalformedURLException e) {
+                                                e.printStackTrace();
+                                            }
+                                            String name = object.getString("name");
+                                            String email = object.getString("email");
+                                            String gender = object.getString("gender");
+                                            String birthday = object.getString("birthday");
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields",
+                                "id,name,email,gender, birthday");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        System.out.println("onCancel");
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        System.out.println("onError");
+                        Log.v("LoginActivity", exception.getCause().toString());
+                    }
+                });
+
+
         initialiseViews();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, responseCode, data);
+        callbackManager.onActivityResult(requestCode, responseCode, data);
+    }
+
 
     private void initialiseViews() {
         Button btnGoogleSignIn = (Button) findViewById(R.id.btnGoogleSignIn);
@@ -50,6 +147,10 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
                 frameLayout.setVisibility(View.VISIBLE);
                 addFragment();
                 break;
+            case R.id.btnFacebookSignIn:
+                loginButton.performClick();
+                break;
+
         }
     }
 }
