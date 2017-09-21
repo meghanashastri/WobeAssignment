0e1520a49770b13cbea5f7ec6da77355545cdfda
Meghana <meghanashastri07@gmail.com>
2017-09-21 11:44:54 +0530

    Application Loader class added

---
+++ app/src/main/java/com/example/admin/wobeassignment/ApplicationLoader.java
@@ -0,0 +1,34 @@
+package com.example.admin.wobeassignment;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Admin on 21-09-2017.
+ */
+
+public class ApplicationLoader extends Application {
+
+    public static final String TAG = ApplicationLoader.class.getSimpleName();
+    private static ApplicationLoader mInstance;
+    private static RequestQueue mRequestQueue;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public static synchronized ApplicationLoader getInstance() {
+        return mInstance;
+    }
+
+    public static RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(ApplicationLoader.getInstance());
+        }
+
+        return mRequestQueue;
+    }
+}
