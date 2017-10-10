053197440146e12cecf9e3d9ba9d368383359058
Meghana <meghanashastri07@gmail.com>
2017-10-10 15:23:55 +0530

    implementing api with scan

--- app/src/main/java/com/example/admin/wobeassignment/activities/ScanQRCode.java
+++ app/src/main/java/com/example/admin/wobeassignment/activities/ScanQRCode.java
@@ -1,24 +1,53 @@
 package com.example.admin.wobeassignment.activities;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.admin.wobeassignment.ApplicationLoader;
 import com.example.admin.wobeassignment.R;
+import com.example.admin.wobeassignment.model.BaseModel;
+import com.example.admin.wobeassignment.model.VerifyUserModel;
+import com.example.admin.wobeassignment.utilities.CommonUtils;
+import com.example.admin.wobeassignment.utilities.Constants;
+import com.example.admin.wobeassignment.utilities.FontManager;
+import com.example.admin.wobeassignment.utilities.SharedPreferenceManager;
+import com.google.gson.Gson;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.math.BigInteger;
+
 /**
  * Created by Admin on 10-10-2017.
  */
 
-public class ScanQRCode extends AppCompatActivity {
+public class ScanQRCode extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etEmail, etCredits, etDescription;
+    private String toCustomerId, fromCustomerId, toFirstName, toLastName, fromFirstName, fromLastName;
+    private Button btnPay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +55,8 @@ public class ScanQRCode extends AppCompatActivity {
         setContentView(R.layout.activity_scan_qr_code);
         initialiseToolbar();
         initialiseScan();
+        initialiseViews();
+        fromCustomerId = SharedPreferenceManager.getInstance(this).getString(Constants.CUSTOMER_ID);
     }
 
     /*
@@ -46,6 +77,14 @@ public class ScanQRCode extends AppCompatActivity {
         integrator.setPrompt("Scan barcode");
         integrator.setOrientationLocked(false);
         integrator.initiateScan();
+    }
+
+    private void initialiseViews() {
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etCredits = (EditText) findViewById(R.id.etCredits);
+        etDescription = (EditText) findViewById(R.id.etDescription);
+        btnPay = (Button) findViewById(R.id.btnPay);
+        btnPay.setOnClickListener(this);
     }
 
     /*
@@ -74,9 +113,10 @@ public class ScanQRCode extends AppCompatActivity {
                 try {
                     //converting the data to json
                     JSONObject obj = new JSONObject(result.getContents());
-                    //setting values to textviews
-                    //textViewName.setText(obj.getString("email"));
-                    Toast.makeText(this, obj.getString("email"), Toast.LENGTH_SHORT).show();
+                    //setting values to view
+                    etEmail.setText(obj.getString(Constants.EMAIL));
+                    verifyUserApiCall(obj.getString(Constants.EMAIL));
+
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -85,10 +125,187 @@ public class ScanQRCode extends AppCompatActivity {
                     //in this case you can display whatever data is available on the qrcode
                     //to a toast
                     Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                    etEmail.setText(result.getContents());
+                    verifyUserApiCall(result.getContents());
                 }
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btnPay:
+                sendCreditsValidation();
+                break;
+        }
+    }
+
+
+    /*
+    Method for User validation API call.
+    Request parameters - email.
+    Successful response - toCustomerId, toFirstName, toLastname
+  */
+    private void verifyUserApiCall(final String email) {
+        String url = String.format(Constants.VERIFY_USER_URL, email);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if (response != null && response.getString("returnStatus").equalsIgnoreCase("SUCCESS")) {
+                                VerifyUserModel model = new Gson().fromJson
+                                        (response.toString(), VerifyUserModel.class);
+                                toCustomerId = model.getCUSTOMER_ID().toString();
+                                etEmail.setEnabled(false);
+                                etCredits.setEnabled(true);
+                                etCredits.setHintTextColor(getResources().getColor(R.color.colorPrimary));
+                                etDescription.setEnabled(true);
+                                etDescription.setHintTextColor(getResources().getColor(R.color.colorPrimary));
+                                btnPay.setEnabled(true);
+                                etEmail.setTextColor(getResources().getColor(R.color.edit_text_disable_color));
+                                toFirstName = model.getFIRST_NAME();
+                                toLastName = model.getLAST_NAME();
+                                CommonUtils.firebaseAnalytics("Verified", "User Verified");
+                            } else {
+                                Toast.makeText(ScanQRCode.this, getResources().
+                                                getString(R.string.invalid_user),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        ApplicationLoader.getRequestQueue().add(jsonObjectRequest);
+    }
+
+
+    /*
+     Method for credits entered and description entered validation
+   */
+    private void sendCreditsValidation() {
+        if (!(etCredits.getText().toString().trim().length() > 0)) {
+            Toast.makeText(this, getResources().getText(R.string.enter_cedits), Toast.LENGTH_SHORT).show();
+        } else if (!(etDescription.getText().toString().trim().length() > 0)) {
+            Toast.makeText(this, getResources().getText(R.string.enter_description), Toast.LENGTH_SHORT).show();
+        } else {
+            checkCreditBalance(etCredits.getText().toString().trim());
+        }
+    }
+
+    /*
+    Method to check credit balance with the entered credits.
+    If entered credit <= the credit balance, then Send Credits API call is done.
+    else, customer is indicated that the balance is insufficient
+   */
+    private void checkCreditBalance(String credits) {
+        BigInteger toCredits = BigInteger.valueOf(Long.valueOf(credits));
+        BigInteger userCredits = BigInteger.valueOf(Long.valueOf(SharedPreferenceManager.
+                getInstance(this).getString(Constants.CREDITS)));
+        int availableCredits = userCredits.compareTo(toCredits);
+        if (availableCredits == 0) {
+            if (CommonUtils.isConnectingToInternet(ScanQRCode.this)) {
+                btnPay.setEnabled(false);
+                sendCreditsApiCall(etCredits.getText().toString().trim(), etDescription.getText().toString());
+            } else {
+                Toast.makeText(this, getResources().getString(R.string.check_internet_connection),
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else if (availableCredits == 1) {
+            if (CommonUtils.isConnectingToInternet(ScanQRCode.this)) {
+                btnPay.setEnabled(false);
+                sendCreditsApiCall(etCredits.getText().toString().trim(), etDescription.getText().toString());
+            } else {
+                Toast.makeText(this, getResources().getString(R.string.check_internet_connection),
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else if (availableCredits == -1) {
+            Toast.makeText(this, getResources().getString(R.string.insufficient_credits), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    /*
+     Method to make Send Credits API call.
+
+     Request parameters - fromCustomerId, fromFirstname, fromLastname,
+     toCustomerId, toFirstname, toLastname, credits, description, noteToSelf
+
+     Successful response - Success Message
+  */
+    private void sendCreditsApiCall(final String credits, String description) {
+        if (SharedPreferenceManager.getInstance(this).getString(Constants.FIRST_NAME) != null &&
+                SharedPreferenceManager.getInstance(this).getString(Constants.LAST_NAME) != null) {
+            fromFirstName = SharedPreferenceManager.getInstance(this).getString(Constants.FIRST_NAME);
+            fromLastName = SharedPreferenceManager.getInstance(this).getString(Constants.LAST_NAME);
+        }
+        if (SharedPreferenceManager.getInstance(this).getString(Constants.LAST_NAME) != null) {
+            fromLastName = SharedPreferenceManager.getInstance(this).getString(Constants.LAST_NAME);
+        }
+
+        String url = String.format(Constants.SEND_CREDITS_URL, fromCustomerId, fromFirstName, fromLastName,
+                toCustomerId, toFirstName, toLastName, credits, description, null);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if (response != null && response.getString("returnStatus").equalsIgnoreCase("SUCCESS")) {
+                                BaseModel model = new Gson().fromJson
+                                        (response.toString(), BaseModel.class);
+                                CommonUtils.firebaseAnalytics("Send Credits", credits);
+                                showSuccessDialog();
+                            } else {
+                                Toast.makeText(ScanQRCode.this, getResources().getString(R.string.error_message),
+                                        Toast.LENGTH_SHORT).show();
+                                btnPay.setEnabled(true);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                btnPay.setEnabled(true);
+            }
+        });
+        ApplicationLoader.getRequestQueue().add(jsonObjectRequest);
+    }
+
+    /*
+       Method to show success dialog
+    */
+    private void showSuccessDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        LayoutInflater inflater = (LayoutInflater) this.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.custom_dialog, null);
+        Typeface iconFont = FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
+        TextView success = (TextView) view.findViewById(R.id.success);
+        success.setTypeface(iconFont);
+        Button ok = (Button) view.findViewById(R.id.btnOk);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+        dialog.setContentView(view);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
 }
